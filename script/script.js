9b69be9db0c46263c98781e592b689b9504e3d4a
lucasarrastialodi-tech <lucasarrastialodi@gmail.com>
2025-12-01 17:02:10 -0300

    Update image array in script.js
    Replaced duplicate entry with a new image in the array.

--- script/script.js
+++ script/script.js
@@ -16,7 +16,7 @@ const fotos = [
   { src: "images/princesa0111.jpg", alt: "princesa 01/11/2025" },  
   { src: "images/claudia1.jpg", alt: "mascote claudia" },
   { src: "images/IMG-20251201-WA0019.jpg", alt: "boca de foca" },
-  { src: "images/claudia1.jpg", alt: "mascote claudia" }
+  { src: "images/IMG-20251201-WA0021.jpg", alt: "vichy" }
 ]; // <--- O ERRO ESTAVA AQUI: Faltava fechar o array com ];
 
 const galeria = document.getElementById("galeria-dinamica");
